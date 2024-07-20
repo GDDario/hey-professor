7350000bccd4cd163a2f71dc4ac614ae8fd60103
GDDario <gd.dario.dev@gmail.com>
2024-07-19 21:16:43 -0300

    HEY-9: creating seeder and factory for question model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{Question, User};
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        Question::factory()->count(10)->create();
 
         User::factory()->create([
             'name'  => 'Test User',

3699046b669087a6d1a4923a815f3d703115b88f
GDDario <gd.dario.dev@gmail.com>
2024-08-04 17:53:13 -0300

    hey-10: creating a like controller

---
+++ app/Http/Controllers/Question/LikeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\{RedirectResponse};
+
+class LikeController extends Controller
+{
+    public function __invoke(): RedirectResponse
+    {
+
+        return back();
+    }
+}
